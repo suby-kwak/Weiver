f707734c98b2a7464bd2bf79180fa86e78726c50
suby_kwak <rhkdbtj@gmail.com>
2024-10-25 14:02:37 +0900

    Update maven.yml

--- weiver-main-server/src/main/java/weiver/config/AwsS3Config.java
+++ weiver-main-server/src/main/java/weiver/config/AwsS3Config.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.PropertySource;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@PropertySource("classpath:/application_aws.properties")
 @Configuration 
 public class AwsS3Config {
 
